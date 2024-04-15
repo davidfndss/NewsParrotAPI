a9cd436bbb883a0990c7e9bce53592647d6016fe
DevKing <devdkingg@gmail.com>
2024-04-15 16:41:34 -0300

    fix: /user/check-username

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -8,7 +8,7 @@ const router = Router()
 router.post("/signup", create) // Create user
 router.get("/", authMiddleware, findAll) // Return an Array with all Users found on database
 
-router.get("check-username/", checkUsername) // Checks if the username is available, returns boolean
+router.post("/check-username", checkUsername) // Checks if the username is available, returns boolean
 
 router.get("/:id", validId, validUser, findById) // Return the user with the _id corresponding to the :id param
 router.patch("/:id", validId, validUser, authMiddleware, update) // Updates some fields of the user if approved on validation
