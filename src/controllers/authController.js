d5d1fad1647bddf20e108724f9683bf91f882953
DevKing <devdkingg@gmail.com>
2024-04-18 17:59:28 -0300

    fix: authController.login r

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,8 +3,8 @@ import { loginService } from "../services/authService.js"
 const login = async (req, res) => {
   const { email, password } = req.body
   try {
-    const token = await loginService(email, password)
-    res.status(200).send({token: token})
+    const logUserResponse = await loginService(email, password)
+    res.status(200).send(logUserResponse)
   } catch (err) {
     res.status(500).send({message: err.message})
   }
